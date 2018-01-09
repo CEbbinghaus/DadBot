e03142a23940e74bb269f85b4c7c18eea7e91bef
Unknown <jecebbi@gmail.com>
2018-01-09 20:17:30 +1100

    Server default is now set to true

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const d = require("discord.js");
 const s = require("./settings.json")
 const b = new d.Client()
 var a = new Map();
-b.on('ready', () =>{console.log("running");})
+b.on('ready', () =>{console.log("running");b.guilds.forEach(g => {a.set(g.id, true)})})
 b.on('message', m => {
     if(m.author.bot)return;
     if(m.isMentioned(b.user)){
@@ -12,7 +12,7 @@ b.on('message', m => {
             }
         }
         if(m.author.id == m.guild.owner.id || m.author.id == s.id){
-            if(/stahp/ig.test(m)){
+            if(/(stahp) | (stop)/ig.test(m)){
                 a.set(m.guild.id, false);
                 m.reply("ok i will stop");
             }else if(/start/ig.test(m)){
@@ -21,7 +21,7 @@ b.on('message', m => {
             }
         }
     }
-    if(typeof a.get(m.guild.id) != 'bool' || a.get(m.guild.id)){
+    if(a.get(m.guild.id)){
         let k = /\b(im|i'm) (.+)/ig.exec(m);
         if(!k)return;
         m.channel.send(`Hello ${k[2]} i'm Dad`);
@@ -29,7 +29,7 @@ b.on('message', m => {
 })
 b.on('guildCreate', g => {
     g.owner.createDM().then(d => {
-        d.send("heya im Dadbot i will do stupid shit. if you want me to stop just send **@DadBot stahp**, and you want me to resume my shenanigans then use **start** instead of stahp");
+        d.send("heya im Dadbot i will do stupid shit. if you want me to stop just send **@DadBot stop**, and you want me to resume my shenanigans then use **start** instead of stop");
     })
     a.set(g.id, true);
 })
