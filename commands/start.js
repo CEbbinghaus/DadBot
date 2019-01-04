a5efbc5368c2c2f9b22d1230960e16fef8c68da6
Christopher-Robin <jecebbi@gmail.com>
2019-01-04 22:59:35 +1100

    Added Back commands with Deprechiated values to assist in Tranitioning from the old to the New settings System

---
+++ commands/start.js
@@ -0,0 +1,16 @@
+module.exports = {
+  help: {
+    perms: ["ADMINISTRATOR"],
+    owner: false,
+    server: true,
+    name: "start [DEPRECHIATED]",
+    desk: "Starts the bot's replying function"
+  },
+  command: {
+    weight: 500,
+    regex: /start/ig,
+    run: (bot, message, settings) => {
+      message.reply("This Command is Deprechiated. Please use the toggle command instead. use the help command to find out more");
+    }
+  }
+}
---
+++ commands/stop.js
@@ -0,0 +1,16 @@
+module.exports = {
+    help: {
+        perms: ["ADMINISTRATOR"],
+        owner: false,
+        server: true,
+        name: "stop [DEPRECHIATED]",
+        desk: "Stops the bot from replying"
+    },
+    command: {
+        weight: 500,
+        regex: /stop/ig,
+        run: (bot, message, settings) => {
+          message.reply("This Command is Deprechiated. Please use the toggle command instead. use the help command to find out more");
+        }
+    }
+}
